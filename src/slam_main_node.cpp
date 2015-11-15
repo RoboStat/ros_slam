c4314e87e876bcbef3fab8d6793b6439b6c0b375
william <williamseto2@gmail.com>
2015-11-14 19:38:41 -0500

    added rviz stuff

--- src/slam_main_node.cpp
+++ src/slam_main_node.cpp
@@ -7,6 +7,9 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/calib3d.hpp>
 
+#include <ros/ros.h>
+#include <visualization_msgs/Marker.h>
+
 using namespace std;
 
 #define DEBUG 0
@@ -108,11 +111,59 @@ void confirmTrials() {
 }
 
 //////////////////////main///////////////////////////
-int main() {
+int main( int argc, char** argv ) {
 	cv::namedWindow("SLAM", cv::WINDOW_NORMAL);
 
 	int startFrame = 60;
 	bool trialState = true;
+
+	//rviz marker setup
+	ros::init(argc, argv, "slam_traj");
+	ros::NodeHandle n;
+	ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+
+	// draw line b/w points
+	uint32_t shape = visualization_msgs::Marker::LINE_STRIP;
+	visualization_msgs::Marker marker;
+
+	// Set the frame ID and timestamp.  See the TF tutorials for information on these.
+	marker.header.frame_id = "/my_frame";
+	marker.header.stamp = ros::Time::now();
+
+	// Set the namespace and id for this marker.  This serves to create a unique ID
+	// Any marker sent with the same namespace and id will overwrite the old one
+	marker.ns = "basic_shapes";
+	marker.id = 0;
+
+	// Set the marker type to our shape from earlier
+	marker.type = shape;
+
+	// Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+	marker.action = visualization_msgs::Marker::ADD;
+
+	// Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+	marker.pose.position.x = 0;
+	marker.pose.position.y = 0;
+	marker.pose.position.z = 0;
+	marker.pose.orientation.x = 0.0;
+	marker.pose.orientation.y = 0.0;
+	marker.pose.orientation.z = 0.0;
+	marker.pose.orientation.w = 1.0;
+
+	// Set the scale of the marker -- 1x1x1 here means 1m on a side
+	marker.scale.x = 0.01;
+	marker.scale.y = 0.05;
+	marker.scale.z = 1.0;
+
+	// Set the color -- be sure to set alpha to something non-zero!
+	marker.color.r = 0.0f;
+	marker.color.g = 1.0f;
+	marker.color.b = 0.0f;
+	marker.color.a = 1.0;
+
+	marker.lifetime = ros::Duration();
+	//end rviz setup
+
 	for (int i = startFrame; i < 634; i++) {
 		//read image
 		string path = "/home/wenda/Developer/SLAM/";
@@ -187,8 +238,17 @@ int main() {
 
 		//frame info
 		cv::Mat curR,curT;
-		if (!landmarks.empty())
+		if (!landmarks.empty()) {
+
 			graph.getPose(i,curR,curT);
+			geometry_msgs::Point new_p;
+			new_p.x = curT.at<float>(0, 0);
+			new_p.y = curT.at<float>(1, 0);
+			new_p.z = curT.at<float>(2, 0);
+
+			marker.points.push_back(new_p);
+			marker_pub.publish(marker);
+		}
 		cout << "current pose:" << curR <<endl;
 		cout << curT << endl;
 		cout << "end>> landmarks:" << landmarks.size()
